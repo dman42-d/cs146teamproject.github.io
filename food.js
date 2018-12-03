c6634a6bc5f7c078dc0b0ed0c8d139a2c4b3cca2
HungrySama <45015345+HungrySama@users.noreply.github.com>
2018-12-03 16:23:23 -0500

    Add files via upload

---
+++ food.js
@@ -0,0 +1,23 @@
+function bareB(){
+  window.open("https://www.bareburger.com/")
+}
+
+function mamounB(){
+  window.open("http://mamouns.com/")
+}
+
+function chipB(){
+  window.open("https://www.chipotle.com/")
+}
+
+function pierceB(){
+  window.open("http://stevensdining.com/pierce-dining-hall/")
+}
+
+function bennyB(){
+  window.open("https://www.bennytudinospizzeria.com/")
+}
+
+function karmaB(){
+  window.open("http://www.karmakafe.com/")
+}
