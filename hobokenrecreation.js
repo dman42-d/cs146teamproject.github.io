e16a246a5082fb08a4caabb2429ef887db23492f
kevinchmielewski <44705736+kevinchmielewski@users.noreply.github.com>
2018-12-03 17:04:21 -0500

    Update hobokenrecreation.js

--- hobokenrecreation.js
+++ hobokenrecreation.js
@@ -1,1 +1,22 @@
-
+function checkmobile() {
+	var elements = document.getElementsByClassName('text');
+	if ((typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1)){
+		for (var i = 0; i < elements.length; i++) {
+  			var element = elements[i];
+  			element.style.fontSize = "10px";
+}}    	
+}
+	
+window.onscroll = function() {hideDuringScroll()};
+function hideDuringScroll() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+	
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
