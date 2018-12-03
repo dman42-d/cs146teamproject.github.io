d226379b21fb4a586a00fee1dfe80fcaac4a0522
kevinchmielewski <44705736+kevinchmielewski@users.noreply.github.com>
2018-12-03 17:08:35 -0500

    Update hobokenplacestovisit.js

--- hobokenplacestovisit.js
+++ hobokenplacestovisit.js
@@ -1,1 +1,13 @@
-
+window.onscroll = function() {hideDuringScroll()};
+function hideDuringScroll() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+	
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+}
