36fad31cab8fd31f4bbd3429050a9221292641ba
kevinchmielewski <44705736+kevinchmielewski@users.noreply.github.com>
2018-12-04 13:04:24 -0500

    Update entertainment.js

--- entertainment.js
+++ entertainment.js
@@ -1,9 +1,9 @@
 window.onscroll = function() {hideDuringScroll()};
 function hideDuringScroll() {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        document.getElementById("myBtn").style.display = "block";
+        document.getElementById("backtotopbutton").style.display = "block";
     } else {
-        document.getElementById("myBtn").style.display = "none";
+        document.getElementById("backtotopbutton").style.display = "none";
     }
 }
 	
